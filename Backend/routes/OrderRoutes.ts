2adfcbc700918695326c05e23541010065cd0cc7
Veldwin <edwintreny@gmail.com>
2024-06-12 10:21:30 +0200

    [update] OrderRoutes

--- Backend/routes/OrderRoutes.ts
+++ Backend/routes/OrderRoutes.ts
@@ -1,5 +1,5 @@
 // routes/OrderRoutes.ts
-import express  from 'express';
+import express from 'express';
 import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { validateOrder } from '../middlewares/ValidateOrder';
@@ -7,22 +7,22 @@ import { validateOrder } from '../middlewares/ValidateOrder';
 const router = Router();
 const prisma = new PrismaClient();
 
-router.post('/', validateOrder, async (req : express.Request, res : express.Response) => {
+router.post('/', validateOrder, async (req: express.Request, res: express.Response) => {
   const {
     Order_ID,
     Order_Date,
-    Boisson,
-    Sandwitch,
+    Sandwich,
+    Drink,
+    Side_Food,
     Price,
     Delivery_Fees,
     Service_Fees,
-    Id_User,
     Delivery_Hour,
     Delivery_Code,
     Is_Deleted,
     Id_Address,
+    Id_User,
     Id_Order_Status,
-    Id_Deliveryman,
     Id_Restaurant
   } = req.body;
 
@@ -31,24 +31,24 @@ router.post('/', validateOrder, async (req : express.Request, res : express.Resp
       data: {
         Order_ID,
         Order_Date: new Date(Order_Date),
-        Boisson,
-        Sandwitch,
+        Sandwich,
+        Drink,
+        Side_Food,
         Price,
         Delivery_Fees,
         Service_Fees,
-        Id_User,
         Delivery_Hour: new Date(`1970-01-01T${Delivery_Hour}`),
         Delivery_Code,
         Is_Deleted,
         Id_Address,
+        Id_User,
         Id_Order_Status,
-        Id_Deliveryman,
-        Id_Restaurant,
+        Id_Restaurant
       },
     });
     res.status(201).json(order);
   } catch (error) {
-    console.error('Error occurred:', error); 
+    console.error('Error occurred:', error);
     res.status(500).json({ error: 'Failed to create order' });
   }
 });
