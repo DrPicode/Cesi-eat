92c7fb7c5293dcea153fe6e3f543d95c42044200
Veldwin <edwintreny@gmail.com>
2024-06-12 09:40:37 +0200

    correctif bdd

--- create_database.sql
+++ create_database.sql
@@ -1,34 +1,33 @@
-
-CREATE TABLE User(
+CREATE TABLE User_(
    Id_User INT,
-   Username VARCHAR(55) NOT NULL,
+   Lastname VARCHAR(55) NOT NULL,
    Password VARCHAR(50),
-   email VARCHAR(50),
+   Email VARCHAR(50),
    Firstname VARCHAR(55) NOT NULL,
    Phone VARCHAR(50) NOT NULL,
    Status VARCHAR(50),
    Is_Deleted BOOLEAN  NOT NULL,
    PRIMARY KEY(Id_User)
-);  
+);
 
 CREATE TABLE Address(
    Id_Address INT,
    Address VARCHAR(80) NOT NULL,
    Is_Deleted BOOLEAN  NOT NULL,
    PRIMARY KEY(Id_Address)
-);   
+);
 
 CREATE TABLE Order_Status(
    Id_Order_Status INT,
    Order_Status VARCHAR(50) NOT NULL,
    PRIMARY KEY(Id_Order_Status)
-);  
+);
 
 CREATE TABLE Restaurant_Type(
    Id_Type INT,
    Type VARCHAR(50) NOT NULL,
    PRIMARY KEY(Id_Type)
-);   
+);
 
 CREATE TABLE Restaurant(
    Id_Restaurant INT ,
@@ -37,16 +36,16 @@ CREATE TABLE Restaurant(
    Id_Type INT NOT NULL,
    Id_User INT NOT NULL,
    PRIMARY KEY(Id_Restaurant),
-   FOREIGN KEY(Id_Type) REFERENCES Restaurant_Type(Id_Type)
-   FOREIGN KEY(Id_User) REFERENCES User_Account(Id_User)
-);  
+   FOREIGN KEY(Id_Type) REFERENCES Restaurant_Type(Id_Type),
+   FOREIGN KEY(Id_User) REFERENCES User_(Id_User)
+);
 
 CREATE TABLE Orders(
    Order_ID INT,
    Order_Date DATE NOT NULL,
-   Sandwitch VARCHAR(50),
-   Boisson VARCHAR(50),
-   Accompagnement VARCHAR(50),
+   Sandwich VARCHAR(50),
+   Drink VARCHAR(50),
+   Side_Food VARCHAR(50),
    Price DECIMAL(15,2) NOT NULL,
    Delivery_Fees DECIMAL(15,2),
    Service_Fees VARCHAR(50),
@@ -61,17 +60,16 @@ CREATE TABLE Orders(
    FOREIGN KEY(Id_Address) REFERENCES Address(Id_Address),
    FOREIGN KEY(Id_Order_Status) REFERENCES Order_Status(Id_Order_Status),
    FOREIGN KEY(Id_Restaurant) REFERENCES Restaurant(Id_Restaurant),
-   FOREIGN KEY(Id_User) REFERENCES User_Account(Id_User)
+   FOREIGN KEY(Id_User) REFERENCES User_(Id_User)
 ); 
 
 CREATE TABLE Lives_At(
    Id_User INT,
    Id_Address INT,
    PRIMARY KEY(Id_User, Id_Address),
-   FOREIGN KEY(Id_User) REFERENCES User_Account(Id_User),
+   FOREIGN KEY(Id_User) REFERENCES User_(Id_User),
    FOREIGN KEY(Id_Address) REFERENCES Address(Id_Address)
-);  
-
+);
 CREATE TABLE Is_At(
    Id_Restaurant INT,
    Id_Address INT,
@@ -86,6 +84,6 @@ CREATE TABLE delivery(
    Order_ID INT,
    Id_Delivery INT,
    PRIMARY KEY(Id_User, Order_ID),
-   FOREIGN KEY(Id_User) REFERENCES User_Account(Id_User),
+   FOREIGN KEY(Id_User) REFERENCES User_(Id_User),
    FOREIGN KEY(Order_ID) REFERENCES Orders(Order_ID)
-);   
+);
